353a17fe2a1e027174b569f5be8716339ea69d06
kupualordela-create <kupualordela@gmail.com>
2026-08-02 17:55:33 +0000

    assignment completed
    Add isprime function to check if a number is prime.

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,26 @@
 #include <iostream>
 using namespace std;
 
+bool isprime(int num) {
+    if (num < 20) {
+        return false;
+    }
+
+    return true;
+
+}
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (isprime(number)) {
+        cout << number << " is a prime number." << endl;
+    } else {
+        count << number << " is NOT a prime number." << endl;
+    }
+
+    return 0;
+}
